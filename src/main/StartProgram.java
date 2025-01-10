4e80a4c182fcab75f68c3acf7746a0d3ea4dcd10
CooolRyan <74342019+CooolRyan@users.noreply.github.com>
2025-01-10 10:53:54 +0900

    REFACTOR : Connection Logic
    conn 객체 생성자 주입 받는 방식이 아닌 Repository에서 호출 후 close

--- src/main/StartProgram.java
+++ src/main/StartProgram.java
@@ -4,6 +4,7 @@ import controller.ReadController;
 import repository.ReadRepository;
 import service.ReadService;
 import util.DBUtil;
+import util.DataSource;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
@@ -11,15 +12,15 @@ import java.sql.SQLException;
 
 public class StartProgram {
     public StartProgram() throws SQLException {
-
-        try (Connection connection = DBUtil.getConnection()) {
+        try {
             // 의존성 주입
-            ReadRepository readRepository = new ReadRepository(connection);
+            ReadRepository readRepository = new ReadRepository();
             ReadService readService = new ReadService(readRepository);
             ReadController readController = new ReadController(readService);
             // 데이터 출력
 //            readController.displayAllEstates();
+        }catch (Exception e){
+            e.printStackTrace();
         }
-
     }
 }
